96967cf2e62bd1e8efabd533349cad7022178442
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2022-08-29 10:16:28 +0200

    Set up first Elm steps

---
+++ frontend/src/index.js
@@ -0,0 +1,9 @@
+import { Elm } from './Main.elm';
+
+Elm.Main.init({
+  node: document.getElementById('root'),
+  flags: {
+    graphQLEndpoint: process.env.ELM_APP_BACKEND_URL,
+    mainPageURL: process.env.ELM_APP_MAIN_PAGE_URL,
+  }
+});
