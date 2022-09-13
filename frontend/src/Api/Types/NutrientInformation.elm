cb1ac1ad5181b10bfa541ba776997a8b07102914
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2022-09-13 23:48:33 +0200

    Regenerate nutrient information bridge

--- frontend/src/Api/Types/NutrientInformation.elm
+++ frontend/src/Api/Types/NutrientInformation.elm
@@ -6,12 +6,12 @@ import Json.Encode as Encode
 import Api.Types.Amounts exposing (..)
 import Api.Types.NutrientUnit exposing (..)
 
-type alias NutrientInformation = { name: String, symbol: String, unit: NutrientUnit, amounts: Amounts }
+type alias NutrientInformation = { nutrientCode: Int, name: String, symbol: String, unit: NutrientUnit, amounts: Amounts }
 
 
 decoderNutrientInformation : Decode.Decoder NutrientInformation
-decoderNutrientInformation = Decode.succeed NutrientInformation |> required "name" Decode.string |> required "symbol" Decode.string |> required "unit" decoderNutrientUnit |> required "amounts" (Decode.lazy (\_ -> decoderAmounts))
+decoderNutrientInformation = Decode.succeed NutrientInformation |> required "nutrientCode" Decode.int |> required "name" Decode.string |> required "symbol" Decode.string |> required "unit" decoderNutrientUnit |> required "amounts" (Decode.lazy (\_ -> decoderAmounts))
 
 
 encoderNutrientInformation : NutrientInformation -> Encode.Value
-encoderNutrientInformation obj = Encode.object [ ("name", Encode.string obj.name), ("symbol", Encode.string obj.symbol), ("unit", encoderNutrientUnit obj.unit), ("amounts", encoderAmounts obj.amounts) ]
+encoderNutrientInformation obj = Encode.object [ ("nutrientCode", Encode.int obj.nutrientCode), ("name", Encode.string obj.name), ("symbol", Encode.string obj.symbol), ("unit", encoderNutrientUnit obj.unit), ("amounts", encoderAmounts obj.amounts) ]
