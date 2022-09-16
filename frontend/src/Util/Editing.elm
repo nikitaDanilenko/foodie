a8ff4b8ce06d78c19779bb75a792f5464fee8de6
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2022-09-16 23:31:24 +0200

    Remove unused definition

--- frontend/src/Util/Editing.elm
+++ frontend/src/Util/Editing.elm
@@ -15,13 +15,6 @@ updateLens =
     Lens .update (\b a -> { a | update = b })
 
 
-is : (a -> field) -> field -> Either a (Editing a b) -> Bool
-is fieldOf value =
-    Either.unpack
-        (\p -> fieldOf p == value)
-        (\e -> fieldOf e.original == value)
-
-
 field : (a -> field) -> Either a (Editing a b) -> field
 field f =
     Either.unpack f (.original >> f)
