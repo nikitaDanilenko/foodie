e88d06dc8f58df49fcf2b61fba8c29b3edfc0784
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2022-09-17 23:29:27 +0200

    Incorporate error handling (overview)

--- frontend/src/Pages/Overview/Status.elm
+++ frontend/src/Pages/Overview/Status.elm
@@ -1,18 +1,16 @@
-module Pages.Meals.Status exposing (..)
+module Pages.Overview.Status exposing (..)
 
 import Monocle.Lens exposing (Lens)
 
 
 type alias Status =
     { jwt : Bool
-    , meals : Bool
     }
 
 
 initial : Status
 initial =
     { jwt = False
-    , meals = False
     }
 
 
@@ -20,15 +18,12 @@ isFinished : Status -> Bool
 isFinished status =
     List.all identity
         [ status.jwt
-        , status.meals
         ]
 
 
 lenses :
     { jwt : Lens Status Bool
-    , meals : Lens Status Bool
     }
 lenses =
     { jwt = Lens .jwt (\b a -> { a | jwt = b })
-    , meals = Lens .meals (\b a -> { a | meals = b })
     }
