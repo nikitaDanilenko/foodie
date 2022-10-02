7d64e2c195dd654aaee8e8b5cc1d2805c49b9c65
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2022-10-02 23:40:36 +0200

    Finish view for registration requests

--- frontend/src/Pages/Registration/Request/View.elm
+++ frontend/src/Pages/Registration/Request/View.elm
@@ -2,12 +2,13 @@ module Pages.Registration.Request.View exposing (..)
 
 import Basics.Extra exposing (flip)
 import Html exposing (Html, button, div, input, label, text)
-import Html.Attributes exposing (id)
 import Html.Events exposing (onClick, onInput)
 import Html.Events.Extra exposing (onEnter)
+import Monocle.Lens exposing (Lens)
 import Pages.Registration.Request.Page as Page
 import Pages.Util.Style as Style
 import Pages.Util.ValidatedInput as ValidatedInput
+import Util.LensUtil as LensUtil
 
 
 view : Page.Model -> Html Page.Msg
@@ -17,7 +18,9 @@ view model =
             [ label [] [ text "Nickname" ]
             , input
                 [ onInput
-                    (flip (ValidatedInput.lift Page.lenses.nickname).set model >> Page.SetNickname)
+                    (flip (ValidatedInput.lift LensUtil.identityLens).set model.nickname
+                        >> Page.SetNickname
+                    )
                 , onEnter Page.Request
                 , Style.classes.editable
                 ]
@@ -26,12 +29,15 @@ view model =
         , div []
             [ label [] [ text "Email" ]
             , input
-                [ onInput Page.SetEmail
+                [ onInput
+                    (flip (ValidatedInput.lift LensUtil.identityLens).set model.email
+                        >> Page.SetEmail
+                    )
                 , onEnter Page.Request
                 , Style.classes.editable
                 ]
                 []
             ]
         , div []
-            [ button [ onClick Page.Request, Style.classes.button.confirm ] [ text "Log In" ] ]
+            [ button [ onClick Page.Request, Style.classes.button.confirm ] [ text "Register" ] ]
         ]
