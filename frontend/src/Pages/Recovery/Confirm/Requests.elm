4d491840a6ef21432e4a689881d0c5ac0181d384
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2022-10-07 22:52:29 +0200

    Set up recovery confirmation page

---
+++ frontend/src/Pages/Recovery/Confirm/Requests.elm
@@ -0,0 +1,23 @@
+module Pages.Recovery.Confirm.Requests exposing (confirm)
+
+import Api.Auxiliary exposing (JWT)
+import Api.Types.PasswordChangeRequest exposing (encoderPasswordChangeRequest)
+import Configuration exposing (Configuration)
+import Pages.Recovery.Confirm.Page as Page
+import Pages.Util.Links as Links
+import Util.HttpUtil as HttpUtil
+
+
+confirm :
+    { configuration : Configuration
+    , recoveryJwt : JWT
+    , password : String
+    }
+    -> Cmd Page.Msg
+confirm params =
+    HttpUtil.postJsonWithJWT
+        params.recoveryJwt
+        { url = Links.backendPage params.configuration [ "user", "recovery", "confirm" ]
+        , body = encoderPasswordChangeRequest { password = params.password }
+        , expect = HttpUtil.expectWhatever Page.GotConfirmResponse
+        }
